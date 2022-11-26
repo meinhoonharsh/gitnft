f1bf5f2a17357fcac6af2419e6b9037e06c1d04f
meinhoonharsh <harshprogrammer782@gmail.com>
2022-11-26 16:34:43 +0530

    Created ConnectButton

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,13 +1,12 @@
 import React from "react";
+import ConnectButton from "../components/ConnectButton";
 import "./Index.scss";
 
 export default function Index() {
   return (
     <div className="index-page">
       <div className="container">
-        <button className=" button connect-wallet-button">
-          Connect Wallet
-        </button>
+        <ConnectButton />
         <a href="/home">Go to Home Page</a>
       </div>
     </div>
