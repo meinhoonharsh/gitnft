59b9105c6aa5faef4c38c6a657c15aeec4c680d7
meinhoonharsh <harshprogrammer782@gmail.com>
2022-11-26 08:17:09 +0530

    Created Index Page Layout

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,15 @@
 import React from "react";
+import "./Index.scss";
 
 export default function Index() {
-  return <div>Index Page</div>;
+  return (
+    <div className="index-page">
+      <div className="container">
+        <button className=" button connect-wallet-button">
+          Connect Wallet
+        </button>
+        <a href="/home">Go to Home Page</a>
+      </div>
+    </div>
+  );
 }
