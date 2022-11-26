77c65d7b9d6a273b7b46a8f68180a73a28662071
meinhoonharsh <harshprogrammer782@gmail.com>
2022-11-26 08:12:38 +0530

    Fixed Routing Issues

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
-// import R
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import React from "react";
+import Index from "./pages/Index";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+]);
 
 function App() {
   return (
     <>
-      <Home />
+      <RouterProvider router={router} />
     </>
   );
 }
